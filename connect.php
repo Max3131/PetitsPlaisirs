1d74212823a6eed1a190136c3e1df80c3e2b5187
Maxime <maxime.carmouze@univ-tlse3.fr>
2025-04-01 18:44:37 +0200

    ajout des constantes pour se connecter a la BD

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+//Fichier contenant les constantes de connexions
+// A modifier si nécessaire
+define ('NOM', "root");
+define ('PASSE', "");
+define ('SERVEUR', "localhost");
+define ('BD', "petitsplaisirs");
+
+?>
